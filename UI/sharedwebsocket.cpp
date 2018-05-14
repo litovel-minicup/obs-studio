8ebd3c570baa06d604d606fd9d002389763a3c83
Josef Kolář <thejoeejoee@gmail.com>
2018-05-14 22:28:40 +0200

    Finish websocket connection

--- UI/sharedwebsocket.cpp
+++ UI/sharedwebsocket.cpp
@@ -2,23 +2,83 @@
 
 SharedWebsocket::SharedWebsocket(QObject * parent): QObject(parent)
 {
+    using sslErrorSignal = void(QWebSocket::*)(const QList<QSslError>&);
+
 	connect(&m_socket, &QWebSocket::connected, this, &SharedWebsocket::onConnected);
+	connect(&m_socket, &QWebSocket::disconnected, this, &SharedWebsocket::onDisconnected);
+	connect(&m_socket, static_cast<sslErrorSignal>(&QWebSocket::sslErrors), this, &SharedWebsocket::onSslError);
+	connect(this, &SharedWebsocket::urlChanged, this, &SharedWebsocket::handleUrlChange);
 }
 
-void SharedWebsocket::openSocket(const QUrl& url) {
-
+void SharedWebsocket::sendMsg(const QString & msg) {
+    if(m_socket.state() == QAbstractSocket::ConnectedState)
+        m_socket.sendTextMessage(msg);
+    else
+        qWarning() << "trying to send message without connection";
 }
 
-void SharedWebsocket::loopedOpenSocket(const QUrl & url, int time)
-{
+void SharedWebsocket::onConnected() {
+    m_reconnectTimer.stop();
+    this->setStatus(SharedWebsocket::Open);
 
+    connect(&m_socket, &QWebSocket::textMessageReceived, this, &SharedWebsocket::msgReceived);
 }
 
-void SharedWebsocket::sendMsg(const QString & msg)
-{
+void SharedWebsocket::onSslError(const QList<QSslError> &errors) {
+    QString repr;
+
+    for(const QSslError& singleError: errors) {
+        repr += singleError.errorString() + ", ";
+    }
+
+    emit this->error(repr);
 }
 
-void SharedWebsocket::onConnected()
-{
+void SharedWebsocket::setStatus(SharedWebsocket::Statuses status) {
+    if(m_status == status)
+        return;
 
+    m_status = status;
+    emit this->statusChanged(static_cast<int>(status));
+}
+
+int SharedWebsocket::status() const {
+    return static_cast<int>(m_status);
+}
+
+QUrl SharedWebsocket::url() const {
+    return m_url;
+}
+
+void SharedWebsocket::setUrl(const QUrl &url) {
+    if(url == m_url)
+        return;
+    m_url = url;
+    emit this->urlChanged(url);
+}
+
+void SharedWebsocket::handleUrlChange() {
+    m_socket.close();
+    m_reconnectTimer.start();
+    emit this->oldSocketClosed();
+}
+
+void SharedWebsocket::open(std::chrono::milliseconds reconnectTime) {
+    if(reconnectTime == 0ms) {
+        m_socket.open(m_url);
+        return;
+    }
+
+    m_socket.open(m_url);
+    m_reconnectTimer.setInterval(reconnectTime);
+    m_reconnectTimer.start();
+}
+
+void SharedWebsocket::onDisconnected() {
+    m_reconnectTimer.start();
+    this->setStatus(SharedWebsocket::Closed);
+}
+
+void SharedWebsocket::stopReconnecting() {
+    m_reconnectTimer.stop();
 }
--- UI/sharedwebsocket.h
+++ UI/sharedwebsocket.h
@@ -2,28 +2,50 @@
 
 #include <QObject>
 #include <QWebSocket>
+#include <QTimer>
 
 class SharedWebsocket: QObject
 {
         Q_OBJECT
+		Q_ENUMS(Statuses)
+		Q_PROPERTY(int status READ status NOTIFY statusChannged)
+		Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
+
+	public:
+		enum Statuses {Open, Closed};
+
     private:
+		Statuses m_status;
         QWebSocket m_socket;
+        QTimer m_reconnectTimer;
+        QUrl m_url;
 
     public:
-        SharedWebsocket(QObject* parent = nullptr);
+		explicit SharedWebsocket(QObject* parent = nullptr);
+
+		int status() const;
+		QUrl url() const;
 
     private slots:
+		void handleUrlChange();
+		void setStatus(Statuses status);
         void onConnected();
+        void onDisconnected();
+		void onSslError(const QList<QSslError>& errors);
 
 	public slots:
-	    void open(const QUrl& url);
-	    void loopedOpen(const QUrl& url, int time = 1);
+		void stopReconnecting();
+	    void open(std::chrono::milliseconds reconnectTime = 0ms);
 	    void sendMsg(const QString& msg);
+	    void setUrl(const QUrl& url);
 
     signals:
+		void oldSocketClosed();
 	    void connected();
 	    void disconnected();
 	    void error(const QString& msg);
 	    void msgReceived(const QString& msg);
+	    void statusChanged(int status);
+	    void urlChanged(const QUrl& url);
 };
 
